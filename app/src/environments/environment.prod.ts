c04efab8269b2e6f5d910e42052825a18bc52bb6
Nicholas <lesetjamaluleka@gmail.com>
2024-06-10 12:57:39 +0200

    first commit

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "ssdURL": "https://111e-169-239-176-9.ngrok-free.app/api",
+        "tenantName": "neutrinos-delivery",
+        "appName": "fnb-internal",
+        "namespace": "com.neutrinos-delivery.fnb-internal",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB",
+        "isNotificationEnabled": "true",
+        "pushType": "FCM",
+        "firebaseSenderId": "198708922040",
+        "firebaseAuthKey": "BC1HdI3Tgn-ATvZlqW0DkMWuUDGNyUtdWQi9xZWXhLZvir0GZfOLZSq8JAW_ZVBdTYzHGSSjchzRlfjg2ZojO4Q"
+    }
+}
